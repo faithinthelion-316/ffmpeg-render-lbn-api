03d4bfba8492ff4752be15dce3dd41165f36bb83
faithinthelion-316 <faithinthelion@gmail.com>
2026-04-27 22:00:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -252,6 +252,7 @@ def build_title_only_filter(numero_regla: str) -> str:
         (
             f"drawtext="
             f"fontfile='{safe_font_path}':"
+            f"text='VERDAD':"
             f"fontsize=50:"
             f"fontcolor=white:"
             f"borderw=2:"
@@ -264,6 +265,7 @@ def build_title_only_filter(numero_regla: str) -> str:
         (
             f"drawtext="
             f"fontfile='{safe_font_path}':"
+            f"text='#{numero_regla}':"
             f"fontsize=80:"
             f"fontcolor=0xE6C15A:"
             f"borderw=2:"
